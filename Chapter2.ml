56699d2d36f9e989fc1fcf30025b77c05b04d2ef
Liam Wynn <wynnliam@pdx.edu>
2021-05-27 18:39:57 -0700

    Updated explicate control to handle return prim ops (puts them in a variable)

--- Chapter2.ml
+++ Chapter2.ml
@@ -49,48 +49,6 @@ module RemoveComplexOperands =
   struct
     open JVar
 
-    (* Behold this monstrosity I did before! *)
-    (* let rec rco_atom env e =
-    match e with
-    | Prim (Neg, [e]) ->
-      let s = gensym "`tmp" in [(s, rco_exp env e)]
-    | Prim (Add, [Int n1; e]) ->
-      let s = gensym "`tmp" in [(s, rco_exp env e)]
-    | Prim (Add, [e; Int n2]) ->
-      let s = gensym "`tmp" in [(s, rco_exp env e)]
-    | Prim (Add, [e1; e2]) ->
-      let s1 = gensym "`tmp" in
-      let s2 = gensym "`tmp" in
-      [(s1, rco_exp env e1); (s2, rco_exp env e2)]
-    | e -> let s = gensym "`tmp" in [(s, e)]
-
-    and rco_exp env e = e
-      match e with
-      | Int n -> Int n
-      | Var v -> Var v
-      | Prim (Read, []) -> Prim (Read, [])
-
-      | Prim (Neg, [Int n]) -> Prim (Neg, [Int n])
-      | Prim (Neg, [Var v]) -> Prim (Neg, [Var v])
-      | Prim (Neg, [cmplx]) ->
-        let [(name, exp)] = rco_atom env e in
-        Let (name, exp, Prim (Neg, [Var name]))
-
-      | Prim (Add, [Int n1; Int n2]) -> Prim (Add, [Int n1; Int n2])
-      | Prim (Add, [Int n1; Var v2]) -> Prim (Add, [Int n1; Var v2])
-      | Prim (Add, [Var v1; Int n2]) -> Prim (Add, [Var v1; Int n2])
-      | Prim (Add, [Var v1; Var v2]) -> Prim (Add, [Var v1; Var v2])
-      | Prim (Add, [Int n1; cmplx]) ->
-        let [(name, exp)] = rco_atom env e in
-        Let (name, exp, Prim (Add, [Int n1; Var name]))
-      | Prim (Add, [cmplx; Int n2]) ->
-        let [(name, exp)] = rco_atom env e in
-        Let (name, exp, Prim (Add, [Var name; Int n2]))
-      | Prim (Add, [cmplx1; cmplx2]) ->
-        let [(name1, exp1); (name2, exp2)] = rco_atom env e in
-        Let (name1, exp1, Let (name2, exp2, Prim (Add, [Var name1; Var name2])))
-      | exp -> exp*)
-
     let is_atm exp =
     match exp with
     | Int n -> true
@@ -172,20 +130,19 @@ module ExplicateControl =
       match ratm with
       | JVar.Int n -> Int n
       | JVar.Var v -> Var v
-      | _ -> failwith "Trying to convert non-atom!"
+      | _ -> assert false
 
     (* Requires that texp is in tail position *)
     let rec explicate_tail texp =
       match texp with
       | JVar.Int n -> Return (Atom (Int n))
       | JVar.Var v -> Return (Atom (Var v))
-      | JVar.Prim (Neg, [atm]) -> Return (Prim (Neg, [convert_to_catm atm]))
-      | JVar.Prim (Add, [atm1;atm2]) -> Return (Prim (Add, [convert_to_catm atm1; convert_to_catm atm2]))
-      | JVar.Prim (Read, []) -> Return (Prim (Read, []))
+      | JVar.Prim (op, args) ->
+          let v = gensym "`var" in
+          explicate_assign (JVar.Prim (op, args)) v (Return (Atom (Var v)))
       | JVar.Let (x, e1, e2) ->
         let result = explicate_tail e2 in
         explicate_assign e1 x result
-      | _ -> failwith "Unhandled explicate_tail case!"
 
     and explicate_assign exp x cont =
       match exp with
@@ -220,32 +177,6 @@ module ExplicateControl =
 module SelectInstructions =
   struct
     open JasmInt
-
-(*
-type primop =
-    Read
-  | Neg
-  | Add
-
-let arity = function
-    Read -> 0
-  | Neg -> 1
-  | Add -> 2
-
-type atm =
-    Int of int64
-  | Var of var
-
-type exp =
-    Atom of atm
-  | Prim of primop * atm list
-
-type stmt =
-    Assign of var * exp
-
-type tail =
-    Return of exp
-  | Seq of stmt*tail*)
 
     let do_atm a =
       match a with
