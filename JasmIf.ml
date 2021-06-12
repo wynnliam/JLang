7e83bd9914ba109f3159265687b5efe59168d371
Liam Wynn <wynnliam@pdx.edu>
2021-06-12 12:39:04 -0700

    Added Label instruction to JasmIf

--- JasmIf.ml
+++ JasmIf.ml
@@ -1,8 +1,15 @@
 open Util
 
+type label = string
+
 type arg =
   | Imm of Int32.t
-  | Var of string
+  | Var of string (* TODO: Delete this *)
+
+type stack_frame =
+  | Same  (* Same stack frame as ones that jumped to it *)
+  | Append of int (* Same stack frame as ones that jumped to it with n additional locals *)
+  | Stack1 (* Same stack frame as ones that jumped to it with 1 value (an int in our language) on the stack *)
 
 type instr = 
     (* Pushes a long integer constant onto the stack *)
@@ -24,12 +31,21 @@ type instr =
   | InvokeStatic of string
     (* Invokes a virtual method *)
   | Virtual of string
+  | Label of label * stack_frame
 
-type label = string
 type 'pinfo program = Program of 'pinfo * label * instr list
 
 let readn = "read_int.read:\"()I\";"
 let writn = "read_int.write:\"(I)V\";"
+
+let string_of_stack_frame sf =
+  match sf with
+  | Same -> "stack_frame_type: same;"
+  | Append n ->
+      let cnt = take 0 (n - 1) in
+      let cnt' = List.map (fun i -> if i = (n - 1) then "int;" else "int, ") cnt in
+      List.fold_left (fun acc next -> acc ^ next) "stack_frame_type: append;\nlocals_map " cnt'
+  | Stack1 -> "stack_frame_type: stack1;\nstack_map int"
 
 let print_instruction oc intruction =
   match intruction with
@@ -44,6 +60,7 @@ let print_instruction oc intruction =
   | Pop -> Printf.fprintf oc "pop\n"
   | InvokeStatic f -> Printf.fprintf oc "invokestatic Method %s\n" f
   | Virtual v -> Printf.fprintf oc "invokevirtual Method %s\n" v
+  | Label (lbl, sf) -> Printf.fprintf oc "%s:\n%s" lbl (string_of_stack_frame sf)
 
 let rec print_instrs oc instrs =
   match instrs with
