b302fdcebb026e47f88b488d2855dba8a5551904
Liam Wynn <wynnliam@pdx.edu>
2021-08-03 09:39:18 -0700

    Added read_int.java

---
+++ tests/read_int.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class read_int {
+  public static int read() {
+    return new Scanner(System.in).nextInt();
+  }
+
+  public static void write(int i) {
+    System.out.println(Integer.toString(i));
+  }
+}
